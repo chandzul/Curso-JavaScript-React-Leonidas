ccd858bae111613af542f718effe1dfa1dbc4e39
chandzul <dchandzul@gmail.com>
2021-01-20 00:06:27 -0600

    renderizando componentes p2

--- react/components/app.js
+++ react/components/app.js
@@ -1,13 +1,18 @@
 import { Component } from '../lib/react.js';
 import User from './user.js';
+import Wrapper from './wrapper.js';
 
 class App extends Component {
   render() {
     return `
       <div class="app">
-        ${new User({
-          avatar: './images/ash.jpg',
-          name: 'Ash K.'
+        ${new Wrapper({
+          children: `
+            ${new User({
+              avatar: './images/ash.jpg',
+              name: 'Ash K.'
+            }).render()}
+          `
         }).render()}
       </div>
     `;
